a193d204249a0cbd4eb4bc3e2cb1d8bd4ba6b1e8
Kara0016 <clementiledupe@gmail.com>
2022-09-18 08:59:55 +0200

    Create skip command

---
+++ src/Commands/Music/skip.ts
@@ -0,0 +1,44 @@
+import { ApplicationCommandOptionType } from 'discord.js';
+import { BaseCommand } from '../../Classes/Command';
+import { ExtendedClient } from '../../Classes/ExtendedClient';
+import { CommandData } from '../../Types/globals';
+
+export default class SkipCommand extends BaseCommand {
+	constructor (client: ExtendedClient) {
+		super(client, {
+			filename: __filename,
+			description: 'Skip to the next track',
+			botPermissions: ['SendMessages', 'Speak'],
+			memberPermissions: ['UseApplicationCommands', 'Speak'],
+			enabled: true,
+			guildOnly: true,
+			options: [{
+				type: ApplicationCommandOptionType.Number,
+				name: 'to',
+				description: 'The number of tracks to skip',
+				required: false,
+			}]
+		});
+	}
+
+	run ({ interaction, player }: CommandData): void {
+		if(!player) {
+			interaction.reply({
+				embeds: [{
+					description: '❌ | **Nothing is playing right now...**'
+				}]
+			});
+			return;
+		}
+
+		const amount = interaction.options.getNumber('to', false) || 1;
+
+		player.skip(amount);
+
+		interaction.reply({
+			embeds: [{
+				description: `${amount} track${amount > 1 ? 's' : ''} skiped.`
+			}]
+		});
+	}
+}
