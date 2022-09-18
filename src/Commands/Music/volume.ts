6ab7f870fd77bc7f8d6ed849ff8558fada970cbd
Kara0016 <clementiledupe@gmail.com>
2022-09-18 09:36:53 +0200

    Add volume command

---
+++ src/Commands/Music/volume.ts
@@ -0,0 +1,55 @@
+import { ApplicationCommandOptionType } from 'discord.js';
+import { BaseCommand } from '../../Classes/Command';
+import { ExtendedClient } from '../../Classes/ExtendedClient';
+import { CommandData } from '../../Types/globals';
+
+export default class VolumeCommand extends BaseCommand {
+	constructor (client: ExtendedClient) {
+		super(client, {
+			filename: __filename,
+			description: 'Change the volume of the bot.',
+			botPermissions: ['SendMessages', 'Speak'],
+			memberPermissions: ['UseApplicationCommands', 'Speak'],
+			enabled: true,
+			guildOnly: true,
+			options: [{
+				type: ApplicationCommandOptionType.Number,
+				name: 'pourcentage',
+				description: 'The new volume.',
+				required: true,
+				minValue: 1,
+				maxValue: 150
+			}]
+		});
+	}
+
+	run ({ interaction, player }: CommandData): void {
+		if (!player) {
+			interaction.reply({
+				embeds: [{
+					description: '❌ | **Nothing is playing right now...**'
+				}]
+			});
+			return;
+		}
+
+		const value = interaction.options.getNumber('pourcentage', true);
+
+		if(value > 150) {
+			interaction.reply({
+				embeds: [{
+					description: '❌ | **The volume must be bellow 150...**'
+				}]
+			});
+			return;
+		}
+
+		player.filters.setVolume(value, true);
+
+		interaction.reply({
+			embeds: [{
+				description: `Volume: **${value}**`
+			}]
+		});
+	}
+}
